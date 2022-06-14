d53f1013f04b11f391ea26fda571bdc3391d7dc6
arman_orange <arman@orangetoolz.com>
2022-06-14 23:15:42 +0600

    chore : updated README.md file

---
+++ README.md
@@ -0,0 +1,1 @@
+## Recap basic data structures and algorithms using Javascript
--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -6,22 +6,23 @@ const swap = (arr, i, j) => {
 
 const selectionSort = (arr) => {
 	for (let i = 0; i < arr.length; i += 1) {
-		let swapIdx = i;
+		let min = i;
 
-		for (let j = i + 1; j < arr.length; j ++) {
-			if (arr[j] < arr[swapIdx]) {
-				swapIdx = j;
+		for (let j = i + 1; j < arr.length; j += 1) {
+			if (arr[j] < arr[min]) {
+				min = j;
 			}
 		}
 
-		swap(arr, i, swapIdx)
+		swap(arr, i, min)
 	}
 
 	return arr;
 };
 
-const result = selectionSort([3,5,2,1,9,8,78,54,52,11]);
-console.log(result)
+const result = selectionSort([3,5,1,1,2,1,9,8,78,54,52,11]);
+console.log(result) // [1,  2,  3,  5,  8, 9, 11, 52, 54, 78]
+
 
 // TIME COMPLEXITY: O(N^2) || Quadratic time complexity,
 // as nested for loops
